4acbf14c80cdcf5256449ba1b7bef239652b4beb
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-11 22:37:22 +0530

    Replace the img to image component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,28 +1,37 @@
+import Image from "next/image"
 
-
-function Card() {
+function Card({ title }: { title: string }) {
 
   return (
 
-    <div className="max-w-sm bg-white dark:bg-gray-800">
+    <div className="max-w-full bg-white dark:bg-gray-800">
       <a href="#">
-        <img className="rounded-lg w-fit h-[250px] p-2" src="/moon.jpg" alt="moon" />
+        <Image className="rounded-lg p-3" width={1000} height={324} src="/moon.jpg" alt="moon" />
       </a>
 
-      <div className="p-1">
+      <div className="p-3">
 
-        <p className="mb-3 text-sm text-gray-500 dark:text-gray-400"> Here </p>
+        <div className="flex mb-3">
+          <p className="text-sm text-gray-500 dark:text-gray-400"> Mar 10, 2023 </p>
+          <p className="text-sm text-gray-500 dark:text-gray-400 mx-1"> , </p>
+          <p className="text-sm text-gray-500 dark:text-gray-400"> 5 min read </p>
+        </div>
+
 
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
         </a>
 
       </div>
 
-    </div>
+    </div >
 
   )
 
+}
+
+Card.defaultProps = {
+  title: "Noteworthy technology acquisitions 2021"
 }
 
 export default Card
