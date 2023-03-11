689499a389ac0f50db0d9c2219b7f4e4b82d00ca
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-11 22:40:51 +0530

    remove the unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Header from "@/components/Header";
-import Footer from "@/components/Footer"
 import Card from "@/components/Card"
 
 const inter = Inter({ subsets: ['latin'] })
