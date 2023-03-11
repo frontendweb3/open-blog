309de24ea97999e3f0af77a5967a3f7d22d09624
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-11 22:39:34 +0530

    add pagination in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,16 @@
-
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Pagination from "@/components/Pagination";
 
 function Layout({ children }) {
 
 
   return <>
     <Header />
-    <main className="container my-12 mx-auto grid grid-cols-4 gap-5">
-    {children}
+    <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4  lg:grid-cols-3  md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4 ">
+      {children}
     </main>
+    <Pagination />
     <Footer />
   </>
 
