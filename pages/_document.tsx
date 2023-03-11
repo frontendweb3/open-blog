6d34d49054ab55762c06d55f46acca89387dad8d
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-11 22:40:38 +0530

    change the default dark theme to light theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className='dark' lang="en">
+    <Html className='light' lang="en">
       <Head />
       <body>
         <Main />
