548eb95e11d668700044276e19ca0a17d0536e26
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-12 15:56:06 +0530

    change the <a> to link component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaSearch, FaSun, FaRegMoon } from "react-icons/fa";
 import { useTheme } from 'next-themes'
-
+import Link from "next/link";
 function Header() {
   const { theme, setTheme } = useTheme()
 
@@ -9,34 +9,41 @@ function Header() {
 
       <div className="container flex flex-wrap items-center justify-between mx-auto">
 
-        <a href="/" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">OpenBlog</span>
-        </a>
+        </Link>
 
         <div className="flex md:order-2">
+
           <ul className="flex flex-wrap p-4 md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 dark:hover:text-blue-700 rounded md:p-0 dark:text-white" aria-current="page">
+
+              <Link href={"#"}  className="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 dark:hover:text-blue-700 rounded md:p-0 dark:text-white" aria-current="page">
                 <FaSearch />
-              </a>
+              </Link>
+
             </li>
+
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
+              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
                 <FaTwitter />
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded  hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white"> <FaFacebook /> </a>
+              <Link href="#" 
+              className="block py-2 pl-3 pr-4 text-gray-700 rounded  hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white"> <FaFacebook />
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
+              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
                 <FaLinkedin />
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
+              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
                 <FaInstagram />
-              </a>
+              </Link>
             </li>
             <li >
               <button className="block py-2 pl-3 pr-4 rounded md:p-0" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
