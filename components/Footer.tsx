6bb0440dbdcc03a7d33b4295a1a590d8ccce24bd
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-12 15:55:44 +0530

    change the <a> to link component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
 import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+import Link from "next/link";
+
 
 function Footer() {
-
 
   return (
 
@@ -9,27 +10,27 @@ function Footer() {
 
       <div className="container flex flex-wrap items-center justify-between mx-auto">
 
-        <a href="https://github.com/frontendweb3" className="flex items-center">
+        <Link href="https://github.com/frontendweb3" className="flex items-center">
           <span className="self-center text-gray-400 text-sm font-semibold whitespace-nowrap dark:text-white">2023 copyright frontend web</span>
-        </a>
+        </Link>
 
         <div className="flex md:order-2">
 
           <ul className="flex p-4 flex-row md:space-x-8 md:mt-0 md:text-sm font-medium">
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
+              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
                 <FaGithub />
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
+              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
                 <FaTwitter />
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white ">
+              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white ">
                 <FaLinkedin />
-              </a>
+              </Link>
             </li>
 
           </ul>
@@ -37,6 +38,8 @@ function Footer() {
 
       </div>
     </footer>
+
   )
 }
+
 export default Footer
