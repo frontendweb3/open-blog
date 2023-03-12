864b00459737209aa49bb2c2253a8562ec53e291
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-12 15:58:00 +0530

    fix the css issue

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -6,8 +6,8 @@ function Pagination() {
 
       <ul className="mx-auto flex justify-center -space-x-px">
         <li>
-          <a href="#" className="px-3 py-2 ml-0 border border-transparent rounded-md  leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
-            dark:hover:bg-gray-700 dark:hover:text-white">Prev</a>
+          <a href="#" className="px-3 py-2 mr-2 border border-transparent rounded-md  leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
+            dark:hover:bg-gray-700 dark:hover:text-white">  Prev</a>
         </li>
         <li>
           <a href="#" className="px-3 py-2 leading-tight bg-blue-100 hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
@@ -22,7 +22,7 @@ function Pagination() {
           <a href="#" className="px-3 py-2 leading-tight bg-blue-100  hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
         </li>
         <li>
-          <a href="#" className="px-3 py-2 ml-0  border border-transparent rounded-md leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
+          <a href="#" className="px-3 py-2 ml-2  border border-transparent rounded-md leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
             dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
         </li>
 
