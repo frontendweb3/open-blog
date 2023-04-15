3430ce433d71a27dc8562424119e9e6bf096ca92
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-15 22:15:23 +0530

    update the component

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ function ErrorPage() {
     <section className="bg-white dark:bg-gray-900 my-16">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500"> 404 </h1>
           <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white"> Something's wrong</p>
           <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
             Sorry, we can't find that article. You'll find lots to explore on the home page.
