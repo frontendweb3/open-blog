9c54b6f4cb4aecc3afc0fef8e74d6346a039310b
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-15 22:15:38 +0530

    remove the unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,16 @@
-import { Inter } from 'next/font/google'
 import Card from "@/components/Card"
-import Pagination from '@/components/Pagination'
+import Pagination from "@/components/Pagination"
+import { posts } from "@/data/posts";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function home() {
   return (
     <>
-      <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4  lg:grid-cols-3  md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
+
+        {
+          posts.map(item => <Card key={item.id} item={item} />)
+        }
+
       </main>
 
       <Pagination />
