4d35f7acaf8a95ed5981c507b81ea70d7fe0f5e6
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-15 22:12:32 +0530

    change the <a> to Link component

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 function Pagination() {
 
   return (
@@ -6,24 +8,22 @@ function Pagination() {
 
       <ul className="mx-auto flex justify-center -space-x-px">
         <li>
-          <a href="#" className="px-3 py-2 mr-2 border border-transparent rounded-md  leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
-            dark:hover:bg-gray-700 dark:hover:text-white">  Prev</a>
+          <Link href="#" className="px-3 py-2 mr-2 border border-transparent rounded-md  leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">  Prev</Link>
         </li>
         <li>
-          <a href="#" className="px-3 py-2 leading-tight bg-blue-100 hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+          <Link href="#" className="px-3 py-2 leading-tight bg-blue-100 hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">1</Link>
         </li>
         <li>
-          <a href="#" className="px-3 py-2 leading-tight bg-blue-100  hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+          <Link href="#" className="px-3 py-2 leading-tight bg-blue-100  hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">2</Link>
         </li>
         <li>
-          <a href="#" className="px-3 py-2 leading-tight bg-blue-100  hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">3</a>
+          <Link href="#" className="px-3 py-2 leading-tight bg-blue-100  hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">3</Link>
         </li>
         <li>
-          <a href="#" className="px-3 py-2 leading-tight bg-blue-100  hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+          <Link href="#" className="px-3 py-2 leading-tight bg-blue-100  hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">4</Link>
         </li>
         <li>
-          <a href="#" className="px-3 py-2 ml-2  border border-transparent rounded-md leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
-            dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+          <Link href="#" className="px-3 py-2 ml-2  border border-transparent rounded-md leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</Link>
         </li>
 
       </ul>
