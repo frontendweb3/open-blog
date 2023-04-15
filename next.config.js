7304b4fbcf42ef8e908709032e7f2e0b5ee2fcbd
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-15 22:16:22 +0530

    adding image and typescript config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['flowbite.s3.amazonaws.com', 'flowbite.com'],
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
 }
 
 module.exports = nextConfig
