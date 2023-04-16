def6f208c186750d6d9cb9515d70c8059361f773
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-16 22:44:18 +0530

    update the component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,16 @@ import Card from "@/components/Card"
 import Pagination from "@/components/Pagination"
 import { posts } from "@/data/posts";
 
-export default function home() {
+export default function Home() {
   return (
     <>
       <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
 
         {
-          posts.map(item => <Card key={item.id} item={item} />)
+          posts.map(item => <Card
+            key={item.id}
+            item={item}
+          />)
         }
 
       </main>
