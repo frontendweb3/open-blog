b7e793ae1c36efde7a058b3f3570db030cab187c
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-16 22:44:05 +0530

    adding await experiment enable in webpack

--- next.config.js
+++ next.config.js
@@ -6,7 +6,11 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+    return config
+  },
 }
 
 module.exports = nextConfig
