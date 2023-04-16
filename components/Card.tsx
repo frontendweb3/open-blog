ed11ee49278d6cd8f372f7277c69263ac746fc6c
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-16 22:44:49 +0530

    update the card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,42 +1,35 @@
 import Image from "next/image"
 import Link from "next/link";
 
-
-function Card({ title }: { title: string }) {
+function Card({ item }) {
 
   return (
 
     <div className="max-w-full bg-white dark:bg-gray-800">
 
-
-      <Link href="#">
-        <Image className="rounded-lg p-3" width={1000} height={324} src="/moon.jpg" alt="moon" />
-      </Link>
-
+      <Image className="rounded-lg p-3" width={1000} height={324} src={item.image} alt=" demo image " />
 
       <div className="p-3">
 
         <div className="flex mb-3">
+
           <p className="text-sm text-gray-500 dark:text-gray-400"> Mar 10, 2023 </p>
           <p className="text-sm text-gray-500 dark:text-gray-400 mx-1"> , </p>
           <p className="text-sm text-gray-500 dark:text-gray-400"> 5 min read </p>
+
         </div>
 
 
-        <Link href="/read/how-are-you">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+        <Link className="text-lg " href={`/read/${item.title.toLowerCase().trim().split(" ").join("-")}`}>
+          {item.title}
         </Link>
 
       </div>
 
-    </div >
+    </div>
 
   )
 
-}
-
-Card.defaultProps = {
-  title: "Noteworthy technology acquisitions 2021"
 }
 
 export default Card
