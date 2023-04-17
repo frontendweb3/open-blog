8e909fddb5a5ad554c09e48dd63f6a39ea0f2b45
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-17 16:21:41 +0530

    update the page

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 function ErrorPage() {
 
   return (
-    <section className="bg-white dark:bg-gray-900 my-16">
+    <section className="dark:bg-gray-900 my-16">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500"> 404 </h1>
