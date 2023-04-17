12d9bafb40ecd83843fb624228c1493ff7766ef9
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-17 16:21:55 +0530

    adding type support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Card from "@/components/Card"
 import Pagination from "@/components/Pagination"
 import { posts } from "@/data/posts";
+import { type Posts } from "@/type";
 
 export default function Home() {
   return (
@@ -8,10 +9,7 @@ export default function Home() {
       <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
 
         {
-          posts.map(item => <Card
-            key={item.id}
-            item={item}
-          />)
+          posts.map((item: Posts) => <Card key={item.id} item={item} />)
         }
 
       </main>
