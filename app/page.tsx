ff6cdd7660f6d9e158940b1884741ad7dc7d6c06
Rajdeep Singh <radewala.2@gmail.com>
2024-11-23 12:43:03 +0530

    update the metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import { Card } from "@/components/Card"
 import { Pagination } from "@/components/Pagination"
 import { GetAllPosts } from "@/data";
 import type { Posts } from "@/type";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Open Blog",
+  description: "The Open Blog is a open source template is design and built with Next.js 15, Shadcn UI and tailwind CSS.",
+};
+
+
 export default function Home() {
   const allPosts = GetAllPosts()
-
   return (
     <>
       <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
